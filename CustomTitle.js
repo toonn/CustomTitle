34c3e2646bffbc632cce7b65b3bbd8f12c61cbaa
toonn <toonn@toonn.io>
2021-03-03 22:33:45 +0100

    Re-append profile tag when title is updated
    Some pages update their title after we appended the profile tag,
    effectively overwriting it again.
    
    With a MutationObserver we can listen for changes to the title element
    and append the profile tag each time.

--- CustomTitle.js
+++ CustomTitle.js
@@ -1,7 +1,20 @@
-getTag = browser.storage.local.get("ProfileTag");
-getTag.then((res) => {
-  var ProfileTag = res.ProfileTag || "";
-  if (ProfileTag != "" && window.document.title.indexOf(ProfileTag) == -1) {
-      window.document.title += " " + ProfileTag;
-  }
-});
+const appendTag = function() {
+  getTag = browser.storage.local.get("ProfileTag");
+  getTag.then((res) => {
+    var ProfileTag = res.ProfileTag || "";
+    if (ProfileTag != "" && document.title.indexOf(ProfileTag) == -1) {
+        document.title += " " + ProfileTag;
+    }
+  });
+};
+
+appendTag();
+
+const appendTagOnChange = function(mutationsList, observer) {
+  for(const mutation of mutationsList) {
+    appendTag();
+  };
+};
+
+const observer = new MutationObserver(appendTagOnChange);
+observer.observe(document.querySelector("title"), { childList: true });
